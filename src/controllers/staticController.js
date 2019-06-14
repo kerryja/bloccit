4565588e5fe32c7d139852071f79a0f2c5f9925d
Kerry Jackson <kerry.jackson108@gmail.com>
2019-06-14 08:53:42 -0400

    Assignment 2995: completed

--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -1,8 +1,8 @@
 module.exports = {
 	index(req, res, next) {
 		res.render("static/index", { title: "Welcome to Bloccit" });
+	},
+	about(req, res, next) {
+		res.render("static/about", { title: "About Us" });
 	}
-	/*about(req, res, next) {
-		res.render("static/about", { title: "About Us" });*/
-	}
-
+}	
