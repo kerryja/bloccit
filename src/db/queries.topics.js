47c6921156ed89036257dcc825cef08558e0f368
Kerry Jackson <kerry.jackson108@gmail.com>
2019-06-18 15:02:29 -0400

    progress so far

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -3,13 +3,63 @@ const Topic = require("./models").Topic;
 module.exports = {
 
 	getAllTopics(callback) {
-		return Topic.all()
+		return Topic.findAll() //Model.all has been deprecated
 
-		.then((topics) => {
-			callback(null, topics);
+			.then((topics) => {
+				callback(null, topics);
+			})
+			.catch((err) => {
+				callback(err);
+			})
+	},
+	addTopic(newTopic, callback) {
+		return Topic.create({
+			title: newTopic.title,
+			description: newTopic.description
 		})
-		.catch((err) => {
-			callback(err);
+			.then((topic) => {
+				callback(null, topic);
+			})
+			.catch((err) => {
+				callback(err);
+			})
+	},
+	getTopic(id, callback) {
+		return Topic.findByPk(id) //findById has been deprecated
+			.then((topic) => {
+				callback(null, topic);
+			})
+			.catch((err) => {
+				callback(err);
+			})
+	},
+	deleteTopic(id, callback) {
+		return Topic.destroy({
+			where: { id }
 		})
+			.then((topic) => {
+				callback(null, topic);
+			})
+			.catch((err) => {
+				callback(err);
+			})
+	},
+	updateTopic(id, updatedTopic, callback) {
+		return Topic.findByPk(id) //findById has been deprecated
+			.then((topic) => {
+				if (!topic) {
+					return callback("Topic not found");
+				}
+
+				topic.update(updatedTopic, {
+					fields: Object.keys(updatedTopic) //issue
+				})
+					.then(() => {
+						callback(null, topic);
+					})
+					.catch((err) => {
+						callback(err);
+					});
+			});
 	}
-}
+}	
