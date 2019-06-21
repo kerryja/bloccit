c68216a5183e43c8c20743da125da4b621f67e85
Kerry Jackson <kerry.jackson108@gmail.com>
2019-06-21 19:41:46 -0400

    Assignment 3003: completed

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/postController");
+const topicController = require("../controllers/topicController");
 const validation = require("./validation");
 
 router.get("/topics/:topicId/posts/new", postController.new);
 router.post("/topics/:topicId/posts/create", validation.validatePosts, postController.create);
+router.post("/topics/:topicId/topics/create", validation.validatePosts, topicController.create);
 router.get("/topics/:topicId/posts/:id", postController.show);
 router.post("/topics/:topicId/posts/:id/destroy", postController.destroy);
 router.get("/topics/:topicId/posts/:id/edit", postController.edit);
 router.post("/topics/:topicId/posts/:id/update", validation.validatePosts, postController.update);
+router.post("/topics/:topicId/topics/:id/update", validation.validatePosts, topicController.update);
 
 module.exports = router;
