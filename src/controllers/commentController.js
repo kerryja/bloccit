2dd70783dae2f64cfe336f3f4b144d5ce7d345c1
Kerry Jackson <kerry.jackson108@gmail.com>
2019-07-12 19:44:11 -0400

    CSS updates and error in commentController fixed

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -22,7 +22,7 @@ module.exports = {
       });
     } else {
       req.flash("notice", "You must be signed in to do that.")
-      req.redirect("/users/sign_in");
+      res.redirect("/users/sign_in");
     }
   },
 
